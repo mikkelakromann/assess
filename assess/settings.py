fcfadc54499c7cd9300bbed357683d6f98d92020
mikkelakromann <mikr@cowi.com>
2019-02-08 17:04:03 +0100

    Fixed error in template dir settings.py introduced by upgrading to new django version.

--- assess/settings.py
+++ assess/settings.py
@@ -58,9 +58,9 @@ ROOT_URLCONF = 'assess.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR + '/templates/',
-        ],
+        'DIRS': [ os.path.join(BASE_DIR,'templates') ],
+#            BASE_DIR + '/templates/',
+#        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
