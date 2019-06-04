bf84d1744b50e146f46affbcc150e0b416961eb6
mikkelakromann <mikr@cowi.com>
2019-06-04 17:04:27 +0200

    Refactored tableIO.py to use keys.py calculation of model headers and column field.

--- base/table.py
+++ base/table.py
@@ -24,24 +24,11 @@ class AssessTable():
         self.version = Version()
         self.version.set_version_id(version)
 
-
-        # Collections support tables with one or more index fields, zero or one
-        # column fields and exactly one value field
-        # Fields are either an index field or the value field
-        self.index_fields = model.index_fields  # list: Index field names
-        self.value_field = model.value_field    # String: Value field name
-
-        # column_field should be value_field or an element in index_fields
-        self.column_field = model.column_field  # String: Column field name
-
         # Records are kept in a dict with values of model objects
         # The record dict keys are tuples of the index fields
         self.records = {}                       # Dict of model objects
         self.records_changed = {}               # Dict of changed model objects
 
-        # Lists for rendering table in template: headers is for the template
-        self.item_headers = []                  # List of item header strings
-        self.index_headers = []                 # List of index header strings
         # Each row is a dict with keys from self.header and values from DB
         self.rows = []                          # List of row dicts
 
@@ -53,9 +40,9 @@ class AssessTable():
         context['table_model_name'] = self.model_name
         if not self.rows == []:
             context['row_list'] = self.rows
-            context['header_list'] = self.index_headers + self.value_headers
-            context['header_list_index'] = self.index_headers
-            context['header_list_items'] = self.value_headers
+            context['header_list'] = self.keys.index_headers + self.keys.value_headers
+            context['header_list_index'] = self.keys.index_headers
+            context['header_list_items'] = self.keys.value_headers
             history = History(self.model)
             context['history'] = history.context_data
             context['version_link_id'] = self.version.link_id
@@ -72,27 +59,26 @@ class AssessTable():
         """Pivot table and populate self.rows with table for Django template"""
 
         # Calculate the table's column headers and row keys
-        keys = Keys(self.model)
-        keys.set_headers(column_field)
-        self.index_headers = keys.index_headers
-        self.value_headers = keys.value_headers
-        # OBS: Implement ordering of index columns at some point
-        key_list = keys.get_key_list()
+        self.keys = Keys(self.model)
+        self.keys.set_headers(column_field)
+        # OBS: Perhaps implement ordering of index columns at some point
+        # (or rely on jQuery functionality if implemented?)
+        key_list = self.keys.get_key_list()
 
         # keys.get_key_list(): List of tuples - [(item1,itemX),(item2,itemX)]
         for key in key_list:
             # The row is a dict of header names and cell values for that row 
             # Add index_headers and index values (from key) to the row 
-            row = dict(zip(keys.index_headers,key))
+            row = dict(zip(self.keys.index_headers,key))
             # Add value cells to row by looking up the value in self.records 
-            for value_header in keys.value_headers:
+            for value_header in self.keys.value_headers:
                 # The record key also contains the value_header key, and its
                 # index_fields are always sorted by self.model.index_fields
                 record_key = ()
                 for index_field in self.model.index_fields:
-                    if index_field == keys.column_field:
+                    if index_field == self.keys.column_field:
                         record_key += (value_header,)
-                    elif index_field in keys.index_headers:
+                    elif index_field in self.keys.index_headers:
                         record_key += (row[index_field],)
                 record_key += (self.model.value_field,)
                 try:
--- base/tableIO.py
+++ base/tableIO.py
@@ -29,51 +29,12 @@ class AssessTableIO():
 
         # An IO datatable column is either a index or value column
         # depending whether the cell context is an index item or a value
-        self.model_index_headers = []   # Index headers from DB model
-        self.model_value_headers = []   # Value headers from DB model
         self.table_index_headers = []   # Index headers from table
         self.table_value_headers = []   # Value headers from table
-        self.table_one_column = True    # Is the table one-column value?
-        self.column_field = ""          # Column field (to be calculated later)
-        self.value_field = model.value_field
 
         self.rows = []                  # Rows is a list of header/value dicts
         self.keys = Keys(model)         # Model key lookup for the record dict
         self.records = {}               # The table as dict (key/model_object)
-
-
-
-    def set_column_field(self, column_field="") -> None:
-        """Set the column field name according to user or model default"""
-
-        # Sanity check of user supplied column_field
-        model_fields = self.model.index_fields.copy()
-        model_fields.append(self.model.value_field)
-        if column_field not in model_fields:
-            column_field = ""
-
-        # Set user supplied or model default column_field
-        if column_field != "":
-            self.column_field = column_field
-        else:
-            self.column_field = self.model.column_field
-
-        # Calculate the model's and the IO table's headers from column_field
-        # If our input table has a column_field which is a database index field
-        # there will not be a column with that field name in our input table
-        for index_field in self.model.index_fields:
-            if index_field != self.column_field:
-                self.model_index_headers.append(index_field)
-            else:
-            # A multi-value_column table has column_field's items
-                item_labels = self.keys.indices_labels[index_field]
-                self.model_value_headers = item_labels
-                self.table_one_column = False
-
-        # A one-value_column table has [value_field] as column_items
-        if self.column_field == self.model.value_field:
-            self.model_value_headers = list(self.model.value_field)
-            self.table_one_column = True
 
 
     def parse_excel(self):
@@ -87,7 +48,7 @@ class AssessTableIO():
 
         csv_string = request.POST['csv_string']
         column_field = request.POST['column_field']
-        self.set_column_field(column_field)
+        self.keys.set_headers(column_field)
 
         lines = csv_string.splitlines()
         csv_header = lines.pop(0)
@@ -96,7 +57,7 @@ class AssessTableIO():
         table_field_names = csv_header.split(delimiters['sep'])
         table_column_count = len(table_field_names)
         for field in table_field_names:
-            if field in self.model_index_headers:
+            if field in self.keys.index_headers:
                 self.table_index_headers.append(field)
             else:
                 self.table_value_headers.append(field)
@@ -124,17 +85,17 @@ class AssessTableIO():
 
         # Database and table index fields need to be identical sets
         # (we have already sorted the column_field name out of index_fields)
-        if set(self.model_index_headers) != set(self.table_index_headers):
+        if set(self.keys.index_headers) != set(self.table_index_headers):
             return "Model index fields mismatch against user input index fields."
 
         # For one-value_column tables, table_value_field == model_value_field
-        if self.table_one_column:
-            if self.table_value_headers != self.model_value_headers:
+        if self.keys.table_one_column:
+            if self.table_value_headers != self.keys.value_headers:
                 return "The user input value header does not match the model."
         # For multi-value_column tables, table_value_headers must be subsets
         # of the column_field items
         else:
-            if not set(self.table_value_headers).issubset(set(self.model_value_headers)):
+            if not set(self.table_value_headers).issubset(set(self.keys.value_headers)):
                 return "The user input value headers do not match the items."
 
 
@@ -146,9 +107,10 @@ class AssessTableIO():
             index_keys = {}
             index_dict = {}
 
-            value_field = self.value_field
-            column_field = self.column_field
+            value_field = self.model.value_field
+            column_field = self.keys.column_field
 
+            # Create dicts for index field item keys and ids 
             for field in self.table_index_headers:
                 cell = row[field]
                 index_keys[field] = cell
@@ -163,7 +125,7 @@ class AssessTableIO():
                 record_keys[value_field] = value_field
                 # In the data models, the value cells are decimal
                 if self.model.model_type == "data_model":
-                    if self.table_one_column:
+                    if self.keys.table_one_column:
                         # In one-column value tables, use the Decimal value
                         record_dict[value_field] = cell
                     else:
@@ -177,11 +139,11 @@ class AssessTableIO():
                 # In the mappings_model the value field is a foreign key
                 elif self.model.model_type == "mappings_model":
                     value_id = self.keys.indices_labels_ids[field][cell]
-                    if self.table_one_column:
+                    if self.keys.table_one_column:
                         record_dict[value_field + "_id"] = value_id
                     else:
                         index_item_id = self.keys.indices_labels_ids[column_field][field]
-                        index_field_name = self.column_field + "_id"
+                        index_field_name = column_field + "_id"
                         record_dict[index_field_name] = index_item_id
                         record_dict[value_field + "_id"] = value_id
 
